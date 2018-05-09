5f808de25b29d6dbd6b21aaf1357b1d3b2a07d65
ipal0 <ipal0can@gmail.com>
2018-05-09 11:27:11 -0400

    replaced autocommit by with statement

--- __init__.py
+++ __init__.py
@@ -3,34 +3,38 @@ import psycopg2
 class db:
 	def __init__(self, dbname, user):
 		self.conn = psycopg2.connect("dbname=%s user=%s" %(dbname,user))
-		self.conn.set_session(autocommit=True)
-		self.cur = self.conn.cursor()
 
 	def read(self, command, args=(), flat=True):
-		self.cur.execute(command, args)
-		f = self.cur.fetchall()
-		if not f or not flat:
-			return f
-		else:
-			if len(f[0]) == 1:
-				f = [i[0] for i in f]
-				if len(f) == 1:
-					f = f[0]
-			return f
+		with self.conn:
+			with self.conn.cursor() as cur:
+				cur.execute(command, args)
+				f = cur.fetchall()
+				if not f or not flat:
+					return f
+				else:
+					if len(f[0]) == 1:
+						f = [i[0] for i in f]
+						if len(f) == 1:
+							f = f[0]
+					return f
 
 	def read1(self, command, args=()):
-		self.cur.execute(command, args)
-		f = self.cur.fetchone()
-		if len(f) == 1:
-			f = f[0]
-		return f
+		with self.conn:
+			with self.conn.cursor() as cur:				
+				cur.execute(command, args)
+				f = cur.fetchone()
+				if len(f) == 1:
+					f = f[0]
+				return f
 	
 	def write(self, command, args=()):
-		self.cur.execute(command, args)
+		with self.conn:
+			with self.conn.cursor() as cur:
+				cur.execute(command, args)
 
 	def clear(self): # Resets the connection
 		self.conn.reset()
 	
 	def __del__(self):
-		self.cur.close()
 		self.conn.close()
+
